a6603f71e4db72ef045611db08c61780aa3c8eb7
George Shao <GeorgeShao123@gmail.com>
2020-04-07 20:09:03 -0400

    Fixed sound

--- arcadeplus/sound.py
+++ arcadeplus/sound.py
@@ -2,33 +2,13 @@
 Sound library.
 """
 
-
+import pyglet
 from pathlib import Path
-from typing import Union
-
-_audiolib = None
-try:
-    import arcadeplus.soloud.soloud as soloud
-
-    _audiolib = soloud.Soloud()
-    _audiolib.init()
-    _audiolib.set_global_volume(10)
-except Exception as e:
-    print(f"Warning, can't initialize soloud {e}. Sound support will be limited.")
 
 
 class Sound:
-    """ This class represents a sound you can play."""
-    def __init__(self, file_name: str, streaming: bool = False):
-        """ Create and load the sound. """
-        self.file_name: str = ""
-        self.wav_file:Union[soloud.WavStream, soloud.Wav]
 
-        self.voice_handle = None # needed for volume control
-
-        if not _audiolib:
-            return
-
+    def __init__(self, file_name: str):
         # If we should pull from local resources, replace with proper path
         if file_name.startswith(":resources:"):
             import os
@@ -38,71 +18,38 @@ class Sound:
         if not Path(file_name).is_file():
             raise FileNotFoundError(f"The sound file '{file_name}' is not a file or can't be read")
         self.file_name = file_name
-        if streaming:
-            self.wav_file = soloud.WavStream()
+        self.player = pyglet.media.load(file_name)
+
+    def play(self):
+        if self.player.is_queued:
+            player = pyglet.media.load(self.file_name)
+            player.play()
         else:
-            self.wav_file = soloud.Wav()
-        self.wav_file.load(self.file_name)
+            self.player.play()
 
-    def play(self, volume=1.0, pan=0.0):
-        """
-        Play the sound.
 
-        :param float volume: Volume, from 0=quiet to 1=loud
-        :param float pan: Pan, from -1=left to 0=centered to 1=right
-        """
-        if not _audiolib:
-            return
+class PlaysoundException(Exception):
+    pass
 
-        self.voice_handle = _audiolib.play(self.wav_file,
-                                           aVolume=volume,
-                                           aPan=pan,
-                                           aPaused=0,
-                                           aBus=0)
 
-    def stop(self):
-        """
-        Stop a currently playing sound.
-        """
-        if not soloud:
-            return
-        self.wav_file.stop()
+def _load_sound_library():
+    """
+    Special code for Windows so we grab the proper library from our directory.
+    Otherwise hope the correct package is installed.
+    """
 
-    def get_length(self):
-        """ Get length of audio in seconds """
-        if not soloud:
-            return 0
-        return self.wav_file.get_length()
+    import pyglet_ffmpeg2
+    pyglet_ffmpeg2.load_ffmpeg()
 
-    # --- These functions should work, but instead just return zero or otherwise
-    # don't appear functional.
 
-    def get_volume(self):
-        """ Get the current volume """
-        if not _audiolib:
-            return 0
-        return _audiolib.get_volume(self.voice_handle)
-
-    def set_volume(self, volume):
-        """ Set the current volume. """
-        if not _audiolib:
-            return
-        _audiolib.set_volume(self.voice_handle, volume)
-
-    def set_left_right_volume(self, left_volume, right_volume):
-        """ Set absolute left/right volume """
-        if not _audiolib:
-            return
-        _audiolib.set_pan_absolute(self.voice_handle, left_volume, right_volume)
-
-    def get_stream_position(self):
-        """ This always returns zero for some unknown reason. """
-        return _audiolib.get_stream_position(self.voice_handle)
+# Initialize static function variable
+_load_sound_library._sound_library_loaded = False  # type: ignore # dynamic attribute on function obj
 
 
 def load_sound(file_name: str):
     """
-    Load a sound.
+    Load a sound. Support for .wav files. If ffmpeg is available, will work
+    with ogg and mp3 as well.
 
     :param str file_name: Name of the sound file to load.
 
@@ -113,8 +60,8 @@ def load_sound(file_name: str):
     try:
         sound = Sound(file_name)
         return sound
-    except Exception as ex:
-        print(f"Unable to load sound file: \"{file_name}\". Exception: {ex}")
+    except Exception as e:
+        print(f"Unable to load sound file: \"{file_name}\". Exception: {e}")
         return None
 
 
@@ -133,15 +80,18 @@ def play_sound(sound: Sound):
         raise Exception(msg)
     try:
         sound.play()
-    except Exception as ex:
-        print("Error playing sound.", ex)
+    except Exception as e:
+        print("Error playing sound.", e)
 
 
-def stop_sound(sound: Sound):
+def stop_sound(sound: pyglet.media.Source):
     """
     Stop a sound that is currently playing.
 
     :param sound:
     """
     # noinspection PyUnresolvedReferences
-    sound.wav_file.stop()
+    sound.pause()
+
+
+_load_sound_library()
