bfa252f001123241b520b3f7ae5fafb44c45e39d
George Shao <GeorgeShao123@gmail.com>
2020-04-19 09:51:48 -0400

    Updated versionname

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
           author_email="georgeshao123@gmail.com",
           license="gpl-3.0",
           url="https://github.com/GeorgeShao/arcadeplus",
-          download_url="https://github.com/GeorgeShao/arcadeplus/archive/0.6.0.tar.gz",
+          download_url="https://github.com/GeorgeShao/arcadeplus/archive/0.6.1.tar.gz",
           install_requires=install_requires,
           packages=["arcadeplus",
                     "arcadeplus.key",
